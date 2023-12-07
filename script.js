040391dc20ebecaed981fb60dd54e7dcfab4e897
Hall <charliesamhall@icloud.com>
2023-12-07 16:08:51 -0500

    added a button to copy text to clipboard

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ generateBtn.addEventListener("click", writePassword);
 //lowercase
 const lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 //uppercase
-const uppercase = ["A", "B", "C", "D", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+const uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 //numeric
 const numeric = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 //special
